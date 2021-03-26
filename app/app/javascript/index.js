ed10245eae6a7f73d18a84ab48ed0666e03a37d6
gsidburycrawford <66580946+gsidburycrawford@users.noreply.github.com>
2021-03-26 14:15:18 -0400

    message here

---
+++ app/app/javascript/index.js
@@ -0,0 +1,6 @@
+// require("style-loader!css-loader!../css/app.css");
+require("../css/app.css")
+require('./another.js');
+  
+sayHello();
+  
---
+++ app/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: {
+      app: "./app/javascript/index"
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    }
+  };
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader'}
+        ]
+      }
+    ]
+  }
